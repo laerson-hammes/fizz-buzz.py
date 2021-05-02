2edd1d498c8305c5dbcdc6bc167c0bc0ca1ac8c6
laerson-hammes <laersonha@gmail.com>
2021-05-01 23:34:09 -0300

    FizzBuzz

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,9 +1,14 @@
-for number in range(1, 101):
-   if number % 15 == 0:
-      print("FizzBuzz")
-   elif number % 3 == 0:
-      print("Fizz")
-   elif number % 5 == 0:
-      print("Buzz")
-   else:
-      print(number)
+def main(n: int = 101, /) -> None:
+   for number in range(1, n):
+      if number % 15 == 0:
+         print("FizzBuzz")
+      elif number % 3 == 0:
+         print("Fizz")
+      elif number % 5 == 0:
+         print("Buzz")
+      else:
+         print(number)
+      
+
+if __name__ == "__main__":
+   main()
